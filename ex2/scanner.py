f96203cccffb70949baf41373b65d15bf0154c2b
Martin Meciar <mato.meciar@icloud.com>
2022-10-23 23:21:32 +0200

    Change to show closed ports as well

--- ex2/scanner.py
+++ ex2/scanner.py
@@ -7,8 +7,8 @@ if len(sys.argv[1:]) != 1:
 
 nmap = nmap3.Nmap()
 # since it wasn't specified if only open ports are of interest we'll take only the open ones into consideration
-# results = nmap.scan_top_ports(sys.argv[1])
-results = nmap.scan_top_ports(sys.argv[1], args='--open')
+results = nmap.scan_top_ports(sys.argv[1])
+# results = nmap.scan_top_ports(str(sys.argv[1]), args='--open')
 
 for ip in results.keys():
     if ip not in ['runtime', 'stats', 'task_results']:
